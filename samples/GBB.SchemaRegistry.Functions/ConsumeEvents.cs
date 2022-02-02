61b9642976dd653ce00df87f02666522e1df33f5
David Barkol <dabarkol@microsoft.com>
2022-02-01 16:46:30 -0800

    Function for consuming events

---
+++ samples/GBB.SchemaRegistry.Functions/ConsumeEvents.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Data.SchemaRegistry;
+using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Microsoft.Azure.Data.SchemaRegistry.ApacheAvro;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using zohan.schemaregistry.events;
+
+namespace GBB.SchemaRegistry.Functions
+{
+    public class ConsumeEvents
+    {
+        private static readonly SchemaRegistryClient _schemaRegistryClient = InitializeSchemaRegistryClient();
+        private static readonly string _schemaGroup = Environment.GetEnvironmentVariable("SchemaGroup");
+
+        [FunctionName("ConsumeEvents")]
+        public async Task Run(
+            [EventHubTrigger("%EventHubName%", Connection = "EventHubConnectionString")] EventData[] events, 
+            ILogger log)
+        {
+            log.LogInformation("ConsumerEvents function invoked");
+
+            var exceptions = new List<Exception>();
+
+            // Create an instance of the avro encoder
+            var encoder = new SchemaRegistryAvroEncoder(
+                _schemaRegistryClient,
+                _schemaGroup,
+                new SchemaRegistryAvroObjectEncoderOptions { AutoRegisterSchemas = false });
+
+            // Iterate through the collection of events and 
+            // decode each of them with the schema registry
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    // Decode the event
+                    CustomerLoyalty loyalty = (CustomerLoyalty)await encoder.DecodeMessageDataAsync(eventData, typeof(CustomerLoyalty));
+                    log.LogInformation($"Customer ID: {loyalty.CustomerId} - {loyalty.PointsAdded} points added");
+                }
+                catch (Exception e)
+                {
+                    exceptions.Add(e);
+                }
+            }            
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+
+        private static SchemaRegistryClient InitializeSchemaRegistryClient()
+        {
+            var schemaRegistryUrl = Environment.GetEnvironmentVariable("SchemaRegistryUrl");
+            var tenantId = Environment.GetEnvironmentVariable("SchemaRegistryTenantId");
+            var clientId = Environment.GetEnvironmentVariable("SchemaRegistryClientId");
+            var clientSecret = Environment.GetEnvironmentVariable("SchemaRegistryClientSecret");
+            var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+            return new SchemaRegistryClient(schemaRegistryUrl, credential);
+        }
+    }
+}
